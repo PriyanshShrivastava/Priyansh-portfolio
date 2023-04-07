72a739c045c94d8f2d2ccdedda7e6a4112c39f09
PriyanshShrivastava <learn.priyansh@gmail.com>
2023-04-08 02:41:17 +0530

    docs: :sparkles: added homepage and other pages.

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../layout/Layout";
+
+const Blog = () => {
+  return (
+    <Layout>
+      <h2>This is a Blog page</h2>
+    </Layout>
+  );
+};
+
+export default Blog;
---
+++ src/pages/Hompage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../layout/Layout";
+
+const Hompage = () => {
+  return <Layout>Hello</Layout>;
+};
+
+export default Hompage;
